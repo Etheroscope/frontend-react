e3051e75ef5dc054a5c304a33a715282694f752d
dhee103 <dsg115@ic.ac.uk>
2017-10-19 18:21:10 +0100

    [dg][km] Added graphs

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ const favourites = [
   { name: 'The DAO', address: '0xbb9bc244d798123fde783fcc1c72d3bb8c189413' }
 ];
 
+const contracts = {
+   '0xbd897c8885b40d014fb7941b3043b21adcc9ca1c': {
+     address: '0xbd897c8885b40d014fb7941b3043b21adcc9ca1c',
+     variables: [{
+       name: 'total',
+       data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+     }]
+   }
+}
+
+
+
 ReactDOM.render(
-  <EtheroscopeContainer favourites={favourites}/>,
+  <EtheroscopeContainer favourites={favourites} mockContracts={contracts}/>,
   document.getElementById('app')
 );
+
