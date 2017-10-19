a1ee5de5cfee194eabdc51ec49e7e299bbba22da
Maurice Yap <mauriceyap@hotmail.co.uk>
2017-10-19 18:24:03 +0100

    add ssh public key

--- travis-install.sh
+++ travis-install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+echo $ssh_key >> ~/.ssh/authorized_keys/deploy_ssh_key.pub
+echo $ssh_key >> /root/.ssh/authorized_keys/deploy_ssh_key.pub
 pip install git+https://github.com/ansible/ansible.git@devel
