3b89acc511d90f942386d614c1a01ba38ae24076
dhee103 <dsg115@ic.ac.uk>
2017-10-19 18:29:58 +0100

    [dg] Added graph zoom

--- app/scenes/Etheroscope/ContractViewer.js
+++ app/scenes/Etheroscope/ContractViewer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const ReactHighcharts = require('react-highcharts'); // Expects that Highcharts was loaded in the code.
+// const ReactHighcharts = require('react-highcharts'); // Expects that Highcharts was loaded in the code.
+const ReactHighstock = require('react-highcharts/ReactHighstock')
 
 // Highcharts config
 // Load config with api values after clicking on favourite
@@ -15,15 +16,12 @@ class ContractViewer extends React.Component {
   render() {
     return (
       <div>
-        <ReactHighcharts
+        <ReactHighstock
           config={{
             xAxis: {
                 categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
             },
             series: [this.props.contract.variables.find(variable => variable.name === this.state.currentVariable)]
-              // series: [{
-              //     data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
-              // }]
         }}
         />
         <p>Contract viewer</p>
