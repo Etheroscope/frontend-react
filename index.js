cefd9188e21ca71dd1d457f762f7b3e704f68049
Sixte de Maupeou d'Ableiges <sd4215@ic.ac.uk>
2017-10-20 15:51:07 +0100

    Http requests code to get contract variables and get history

--- index.js
+++ index.js
@@ -9,7 +9,59 @@ import EtheroscopeContainer from './app/scenes/Etheroscope'
 const favourites = [
   { name: 'Alice', address: '0xbd897c8885b40d014fb7941b3043b21adcc9ca1c' },
   { name: 'The DAO', address: '0xbb9bc244d798123fde783fcc1c72d3bb8c189413' }
-];
+]
+
+function getContract(address, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        return callback(xmlHttp.response, address)
+      }
+  }
+  const url = `http://etheroscope.alice.si/api/explore/${address}/`
+  xmlHttp.open("GET", url, true); // true for asynchronous 
+  xmlHttp.send(null);
+}
+
+function contractCallback(response, address) {  
+  var abi = JSON.parse(response).abi
+  var items = []
+  abi.forEach(function(item) {
+    if (item.outputs && item.outputs.length === 1 && item.outputs[0].type.indexOf('uint') === 0) {
+      if (item.inputs.length === 0) {
+        items.push(item.name)
+      }
+    }
+  })
+  console.log(items)
+  return {
+    address: address,
+    variables: items
+  }
+}
+
+const getHistory = function (variable, contractAddress) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        return callback(xmlHttp.response)
+      }
+  }
+  const url = 'http://etheroscope.alice.si/api/' + 'getHistory/' + contractAddress + '/' + variable
+  xmlHttp.open("GET", url, true) // true for asynchronous 
+  xmlHttp.send(null)
+}
+
+function historyCallback(response) {
+    console.log(response)
+    const historyData = JSON.parse(response)
+    return {
+      data: historyData,
+      xmin: historyData[0][0],
+      xmax: historyData[historyData.length - 1][0],
+      ymax: historyData[historyData.length - 1][1] 
+    }
+}
 
 const contracts = {
   '0xbd897c8885b40d014fb7941b3043b21adcc9ca1c': {
