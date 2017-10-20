c2d577e4bfd2b5ce94647a890abcedd6f61491c0
Maurice Yap <mauriceyap@hotmail.co.uk>
2017-10-20 10:48:13 +0100

    upload ssh key to Travis for Ansible deployment

--- travis-install.sh
+++ travis-install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-echo $ssh_key >> ~/.ssh/authorized_keys/deploy_ssh_key.pub
-echo $ssh_key >> /root/.ssh/authorized_keys/deploy_ssh_key.pub
 pip install git+https://github.com/ansible/ansible.git@devel
