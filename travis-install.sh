ae7486799868db51a98486ae7046d3ba6921aef4
Maurice Yap <mauriceyap@hotmail.co.uk>
2017-10-22 01:50:16 +0100

    debug copying ansible files

--- travis-install.sh
+++ travis-install.sh
@@ -8,5 +8,10 @@ pip install git+https://github.com/ansible/ansible.git@devel
 echo "Downloading files from platform git repo"
 git clone https://github.com/Etheroscope/platform.git
 echo "Assembling Ansible playbooks"
+ls
+ls platform
 mv platform/ansible .
+ls platform
+ls
 cp -R ansible-additions/. ansible
+ls ansible
