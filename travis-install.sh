ce4857d2aa744a318b71f747fb5b526e48975b3c
Maurice Yap <mauriceyap@hotmail.co.uk>
2017-10-22 01:27:29 +0100

    factor out common Ansible files

--- travis-install.sh
+++ travis-install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# Install Ansible
+echo "Installing Absible"
 pip install git+https://github.com/ansible/ansible.git@devel
+
+# Set up Ansible playbook and related files
+echo "Downloading files from platform git repo"
+git clone https://github.com/Etheroscope/platform.git
+echo "Assembling Ansible playbooks"
+mv platform/ansible .
+cp -R ansible-additions/. ansible
