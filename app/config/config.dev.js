c8894ad3a37a61b1ad226b17a23dc65e92e7119c
Sixte de Maupeou d'Ableiges <sd4215@ic.ac.uk>
2017-11-07 21:17:37 +0000

    Create config for prod and dev

---
+++ app/config/config.dev.js
@@ -0,0 +1,3 @@
+export default {
+    API_BASE_URL: 'dev-api.etheroscope.info'
+}
--- app/scenes/Etheroscope/xhr.js
+++ app/scenes/Etheroscope/xhr.js
@@ -1,16 +1,18 @@
-const API_BASE_URL = 'production-api.etheroscope.info';
+import config from '../config'
+
+const API_BASE_URL = config.API_BASE_URL
 
 export default function fetchJson(url) {
   return new Promise((resolve, reject) => {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", API_BASE_URL + url); 
-    xmlHttp.responseType = 'json';
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.open("GET", API_BASE_URL + url)
+    xmlHttp.responseType = 'json'
     xmlHttp.onreadystatechange = function() { 
       if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        return resolve(xmlHttp.responseText);
+        return resolve(xmlHttp.responseText)
       }
       reject(new Error("Invalid server response"))
     }
-    xmlHttp.send();
+    xmlHttp.send()
   });
 };
