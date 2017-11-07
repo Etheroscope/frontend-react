0af7da0edaff46fe1c9fa6dbfd1886585a183c28
Sixte de Maupeou d'Ableiges <sd4215@ic.ac.uk>
2017-11-07 21:29:57 +0000

    Fix: config.dev.js urls for staging and test

--- app/config/config.dev.js
+++ app/config/config.dev.js
@@ -1,3 +1,9 @@
+// STAGING
 export default {
-    API_BASE_URL: 'dev-api.etheroscope.info'
+    API_BASE_URL: 'staging-api.etheroscope.info'
 }
+
+// TEST
+// export default {
+//     API_BASE_URL: 'test-api.etheroscope.info'
+// }
