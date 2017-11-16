fd93700c85b5df60dbcd68ad070cc2a133df2307
Sixte de Maupeou d'Ableiges <sd4215@ic.ac.uk>
2017-11-16 18:24:41 +0000

    Add route to searchresults. Fix search results page.

--- app/scenes/Etheroscope/components/SearchResults.js
+++ app/scenes/Etheroscope/components/SearchResults.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components'
-import fetchJson from './../xhr'
 
 const Wrapper = styled.div`
   background-color: white;
@@ -9,7 +8,7 @@ const Wrapper = styled.div`
   justify-content: center;
 `
 
-const ContractGrid = styled.div`
+const Page = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
@@ -23,49 +22,53 @@ const ContractGrid = styled.div`
 
 export default class SearchResults extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.displayResult = this.displayResult.bind(this)
+    }
+
     displayResult = (result) => {
         switch (result.category) {
             case 'contract':
                 return (
-                    <li>
-                        <p>Result: {result.name}</p>
-                        <p>Address: {result.address} </p>
-                        <p>Variables:</p>
-                        <ul>
-                            {result.variables.map(variable => <li>{variable}</li>)}
-                        </ul>
-                    </li>
+                  <li>
+                    <p>Result: {result.name}</p>
+                    <p>Address: {result.address} </p>
+                    <p>Variables:</p>
+                    <ul>
+                      {result.variables.map((variable, key) => <li key={key}>{variable}</li>)}
+                    </ul>
+                  </li>
                 )
             case 'address':
                 return (
-                    <div>
-                        <p>Result: {result.address} </p>
-                        <p>Variables:</p>
-                        <ul>
-                            {result.variables.map(variable => <li>{variable}</li>)}
-                        </ul>
-                    </div>
+                  <div>
+                    <p>Result: {result.address} </p>
+                    <p>Variables:</p>
+                    <ul>
+                      {result.variables.map((variable, key) => <li key={key}>{variable}</li>)}
+                    </ul>
+                  </div>
                 )
             case 'organisation':
                 return (
-                    <div>
-                        <p>Organisation: {result.name} </p>
-                        <p>Contracts:</p>
-                        <ul>
-                            {
-                                result.contracts.map(contract => {
-                                    <li>
-                                        {contract.name && <p>Result: {contract.name}</p>}
-                                        <p>Address: {contract.address} </p>
-                                        <p>Variables:</p>
-                                        <ul>
-                                            {contract.variables.map(variable => <li>{variable}</li>)}
-                                        </ul>
-                                    </li>
-                                })
-                            }
-                        </ul>
-                    </div>
+                  <div>
+                    <p>Organisation: {result.name} </p>
+                    <p>Contracts:</p>
+                    <ul>
+                      {result.contracts.map((contract, contractKey) => (
+                        <li key={contractKey}>
+                          {contract.name && <p>Result: {contract.name}</p>}
+                          <p>Address: {contract.address} </p>
+                          <p>Variables:</p>
+                          <ul>
+                            {contract.variables.map((variable, varKey) => <li key={varKey}>{variable}</li>)}
+                          </ul>
+                        </li>
+                            ))
+                      }
+                    </ul>
+                  </div>
                 )
             default: return(<p>No results found</p>)
         }
@@ -74,14 +77,14 @@ export default class SearchResults extends React.Component {
     render() {
         const { searchResult } = this.props
         return (
-            <Wrapper>
-                <Page>
-                    {searchResult 
-                    ? displayResult(searchResult)
+          <Wrapper>
+            <Page>
+              {searchResult 
+                    ? this.displayResult(searchResult)
                     : <p>No results found</p>
                     }
-                </Page>
-            </Wrapper>
+            </Page>
+          </Wrapper>
         )
     }
 }
--- app/scenes/Etheroscope/template.js
+++ app/scenes/Etheroscope/template.js
@@ -2,23 +2,25 @@ import React from 'react'
 import {Router, Route, browserHistory} from 'react-router'
 
 import HomePage from './components/HomePage'
-import Explorer from "./components/Explorer"
+import Explorer from './components/Explorer'
+import SearchResults from './components/SearchResults'
 
 // wrapper to pass props to HomePage
 const HomePageWrapper = (props) => {
     return ( <HomePage {...props} />)
-};
+}
 
 // wrapper to pass props to Explorer
 const ExplorerWrapper = (props) => {
     return ( <Explorer {...props} /> )
-};
+}
 
 const Etheroscope = () => (
   <Router history={browserHistory}>
     <Route path={"/"} component={HomePage} />
     <Route path={"explorer"} component={Explorer} />
+    <Route path={"searchresults"} component={SearchResults} />
   </Router>
-);
+)
 
 export default Etheroscope
