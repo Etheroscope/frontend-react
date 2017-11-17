4c2df843d3f2050fa2db347e76f282bf42c0f030
dhee103 <dsg115@ic.ac.uk>
2017-11-17 10:47:42 +0000

    Made styling consistent

--- app/scenes/Etheroscope/components/ContractViewer.js
+++ app/scenes/Etheroscope/components/ContractViewer.js
@@ -7,77 +7,77 @@ import fetchJson from './../xhr'
 const ReactHighstock = require('react-highcharts/ReactHighstock')
 
 class ContractViewer extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {currentVariable: null, variableData: []}
-        this.variableClicked = this.variableClicked.bind(this)
-    }
+  constructor(props) {
+    super(props)
+    this.state = {currentVariable: null, variableData: []}
+    this.variableClicked = this.variableClicked.bind(this)
+  }
 
-    fetchVariableHistory(varName) {
-        const url = `/contracts/${this.props.contract.address}/history?variable=${varName}`
-        return fetchJson(url)
-    }
+  fetchVariableHistory(varName) {
+    const url = `/contracts/${this.props.contract.address}/history?variable=${varName}`
+    return fetchJson(url)
+  }
 
-    variableClicked(varName) {
-        this.fetchVariableHistory(varName)
-            .then(history => {
-                const processedHistory = history.map(item => {
-                    item.value = parseFloat(item.value)
-                    return [item.time * 1000, item.value]
-                })
-                this.setState({
-                    currentVariable: varName,
-                    variableData: processedHistory.sort()
-                })
-            })
-    }
+  variableClicked(varName) {
+    this.fetchVariableHistory(varName)
+      .then(history => {
+        const processedHistory = history.map(item => {
+          item.value = parseFloat(item.value)
+          return [item.time * 1000, item.value]
+        })
+        this.setState({
+          currentVariable: varName,
+          variableData: processedHistory.sort()
+        })
+      })
+  }
 
-    render() {
-        const variables = this.props.contract.variables
-        const abi = this.props.contract.abi;
-        const nullContract = this.props.contract.nullContract;
+  render() {
+    const variables = this.props.contract.variables
+    const abi = this.props.contract.abi;
+    const nullContract = this.props.contract.nullContract;
 
-        const centeredH = styled.h1`
+    const centeredH = styled.h1`
           text-align: center
         `;
 
-        if ((!abi || abi.length === 0) && !nullContract) {
-            return <centeredH> No ABI for this variable </centeredH>
-        }
-
-        if ((!variables || variables.length === 0) && !nullContract) {
-            return <centeredH> No variables in this contract </centeredH>
-        }
-
-        return (
-            <div>
-                <VariableSelection variables={variables} variableClicked={this.variableClicked}/>
-
-                <ReactHighstock
-                    config={{
-                        rangeSelector: {
-                            selected: 1
-                        },
-                        title: {
-                            text: 'Smart Contract Explorer'
-                        },
-                        series: [
-                            {
-                                name: 'Explorer',
-                                data: this.state.variableData,
-                                tooltip: {
-                                    valueDecimals: 2
-                                }
-                            }
-                        ],
-                        credits: {
-                            enabled: false
-                        }
-                    }}
-                />
-            </div>
-        )
+    if ((!abi || abi.length === 0) && !nullContract) {
+      return <centeredH> No ABI for this variable </centeredH>
     }
+
+    if ((!variables || variables.length === 0) && !nullContract) {
+      return <centeredH> No variables in this contract </centeredH>
+    }
+
+    return (
+      <div>
+        <VariableSelection variables={variables} variableClicked={this.variableClicked}/>
+
+        <ReactHighstock
+          config={{
+            rangeSelector: {
+              selected: 1
+            },
+            title: {
+              text: 'Smart Contract Explorer'
+            },
+            series: [
+              {
+                name: 'Explorer',
+                data: this.state.variableData,
+                tooltip: {
+                  valueDecimals: 2
+                }
+              }
+            ],
+            credits: {
+              enabled: false
+            }
+          }}
+        />
+      </div>
+    )
+  }
 }
 
 export default ContractViewer
