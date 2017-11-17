db23d3771f8088e1e89f42c2d9aeb084025932c3
Sixte de Maupeou d'Ableiges <sd4215@ic.ac.uk>
2017-11-17 11:49:21 +0000

    Add data for organisations on home page

--- app/scenes/Etheroscope/components/ContractCard.js
+++ app/scenes/Etheroscope/components/ContractCard.js
@@ -2,26 +2,44 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Card = styled.div`
-  background-color: gray;
+  background-color: #1998a2;
   border-radius: 4px;
-  width: 33%;
-  height: 150px;
+  width: 49%;
+  height: auto;
   margin-top: 20px;
   margin-bottom: 20px;
+  color: white;
 `
 const Container = styled.div`
   padding-left: 10px;
   padding-right: 10px;
 `
+const Title = styled.h3`
+  text-align: center;
+`
+const Link = styled.a`
+  color: white;
+`
+const ContractList = styled.ul`
+  list-style:none;
+`
 
 class ContractCard extends React.Component {
 
   render() {
-    const { contract } = this.props
+    const { name, description, url, contracts } = this.props.organisation
     return (
       <Card>
         <Container>
-          <p>Contract {contract}</p>
+          <Title>{name}</Title>
+          <p>Description: {description}</p>
+          <p>Website: <Link href={url}>{url}</Link></p>
+          <p>Contracts:</p>
+          <ContractList>
+            {contracts.map((contract, contractKey) => (
+              <Link key={contractKey} href={`/searchresults?${contract}`}><li>{contract}</li></Link>
+            ))}
+          </ContractList>
         </Container>
       </Card>
     )
