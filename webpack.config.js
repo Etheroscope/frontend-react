618c72a719d9e94e198eb523979596779d244e5b
Kian Mayne <git@kianmayne.co.uk>
2017-11-17 05:54:00 +0000

    Fixed webpack configuration setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
 const { resolve } = require('path')
 const merge = require('webpack-merge')
 
-module.exports = ({ NODE_ENV: env }) => {
-  const configs = {
-    production: 'prod',
-    staging: 'staging',
-    testing: 'test',
-    development: 'dev'
-  }
-  const suffix = configs[env] || 'prod'
-  const envConfig = require(`./config/webpack.${suffix}.js`)
+const configs = {
+  production: 'prod',
+  staging: 'staging',
+  testing: 'test',
+  development: 'dev'
+}
+// Pick config based on environment
+const suffix = configs[process.env.NODE_ENV] || 'prod'
+const envConfig = require(`./config/webpack.${suffix}.js`)
 
-  return merge(envConfig, {
-    entry: './index.js',
-    context: __dirname,
-    output: {
-      path: resolve(__dirname, './build'),
-      filename: 'bundle.js',
-      publicPath: '/build/',
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-        { test: /\.css$/, loader: 'style-loader!css-loader' },
-        { test: /(\.eot|\.woff2|\.woff|\.ttf|\.svg)/, loader: 'file-loader' },
-      ],
-    }
-  });
-};
+module.exports = merge(envConfig, {
+  entry: './index.js',
+  context: __dirname,
+  output: {
+    path: resolve(__dirname, './build'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /(\.eot|\.woff2|\.woff|\.ttf|\.svg)/, loader: 'file-loader' }
+    ]
+  }
+})
