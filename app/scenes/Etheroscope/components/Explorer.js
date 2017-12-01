e23da0e16dfe77523930512da1a0f3bb38cdd84c
Diana Avram <dianaavram@dyn1179-66.wlan.ic.ac.uk>
2017-12-01 04:22:18 +0000

    Added modal for email request

--- app/scenes/Etheroscope/components/Explorer.js
+++ app/scenes/Etheroscope/components/Explorer.js
@@ -5,6 +5,10 @@ import fetchJson from './../xhr'
 import AddressFormContainer from './../AddressForm'
 import ContractViewer from './ContractViewer.js'
 import Favourites from './Favourites.js'
+import Modal from 'react-modal'
+import Delay from 'react-delay'
+
+
 
 const Wrapper = styled.div`
   background-color: white;
@@ -36,6 +40,41 @@ const Page = styled.div`
   width: 90%;
   margin: auto;
 `
+const ButtonStyle = styled.button`
+  padding: 10px;
+  width:200px;
+  background-color: rgb(25, 152, 162);
+  border-radius 5px;
+  color: white;
+  display: block;
+  margin: 50px auto;
+
+`
+const InputStyle = styled.input`
+ width:300px;
+`
+const Formstyle=styled.form`
+  margin-left:100px;
+  padding: 10px;
+`
+
+const SubmitButton=styled.button`
+  border-radius 5px;
+  background-color: rgb(25, 152, 162);
+  color: white;
+  margin-left:20px;
+`
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 export default class Explorer extends React.Component {
   constructor(props) {
@@ -43,13 +82,25 @@ export default class Explorer extends React.Component {
     const address = document.location.hash.slice(1);
     this.state = {
       contract: { nullContract: true, variables: [], abi: [] },
-      contractAddress: 'contract address'
+      contractAddress: 'contract address',
+      modalIsOpen: false,
+      email: '',
+       everFocusedEmail: false,
+      inFocus: '',
+
     }
     if (address) this.changeContract(address);
     this.changeContract = this.changeContract.bind(this)
     this.addressChanged = this.addressChanged.bind(this)
     this.exploreClicked = this.exploreClicked.bind(this)
     this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   downloadContract(address) {
@@ -86,6 +137,36 @@ export default class Explorer extends React.Component {
     }
   }
 
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  handleEmailChange(evt) {
+    this.setState({ email: evt.target.value });
+  }
+
+  validEmail() {
+    return validate(this.state.email);
+  }
+
+  handleSubmit(evt) {
+    if (!this.validEmail()) {
+      evt.preventDefault();
+      return;
+    }
+    const { email } = this.state;
+    alert(`Signed up with email: ${email}`);
+  }
+
   render() {
     return (
       <Wrapper>
@@ -102,8 +183,37 @@ export default class Explorer extends React.Component {
         </BannerContainer>
         <Page>
           <ContractViewer contract={this.state.contract} />
+          <Delay wait={1200}>
+            <div>
+              <ButtonStyle onClick={this.openModal}>Taking too long? </ButtonStyle>
+              <Modal
+                isOpen={this.state.modalIsOpen}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                style={customStyles}
+              >
+                <h2 > Give us your email address so we can get back to you!</h2>
+                <Formstyle onSubmit={this.handleSubmit}>
+                  <InputStyle
+                    className={this.validEmail() ? "error" : ""}
+                    type="text"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleEmailChange}
+                  />
+                  <SubmitButton disabled={!this.validEmail()}>Submit</SubmitButton>
+
+                </Formstyle>         
+              </Modal>
+            </div>
+          </Delay>
         </Page>
       </Wrapper>
     )
   }
+}
+
+function validate(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
