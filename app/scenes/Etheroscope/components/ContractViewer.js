68382740bd962025bc069b57b8b863a14e48f7bd
Kian Mayne <git@kianmayne.co.uk>
2018-01-10 14:28:44 +0000

    Clarified no ABI message

--- app/scenes/Etheroscope/components/ContractViewer.js
+++ app/scenes/Etheroscope/components/ContractViewer.js
@@ -224,7 +224,8 @@ class ContractViewer extends React.Component {
             If you are the contract owner, you can upload the source code for
             your contract <a href={`https://etherscan.io/verifyContract?a=${contract.address}`}>here</a>.
             Please note it may take up to 24 hours for the ABI to become
-            available after uploading.
+            available after uploading. Also note that the full source must be
+            available for Etherscan to provide the ABI to our system.
           </p>
         </Centered>)
     } else if (!contract.variables || contract.variables.length === 0) {
